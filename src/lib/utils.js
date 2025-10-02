c0b01f98c00c9eb419c69a364eff72840035478e
Ardit-K <arditkoti7192@gmail.com>
2025-10-02 13:25:22 -0400

    Moved ThemeToggle and Added CN Helper Function

---
+++ src/lib/utils.js
@@ -0,0 +1,8 @@
+import {clsx} from 'clsx';
+import {twMerge} from 'tailwind-merge';
+
+// Utility function to combine class names
+
+export const cn = (...inputs) => {
+    return twMerge(clsx(inputs));
+}
