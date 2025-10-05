7cce2de9794f3b6462d27ce6c7db67a69c29e918
Ardit-K <arditkoti7192@gmail.com>
2025-10-05 15:16:51 -0400

    Footer Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "@/components/SkillsSection";
-import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
+import { ProjectsSection } from "@/components/ProjectsSection";
+import { ContactSection } from "@/components/ContactSection";
+import { Footer } from "@/components/Footer";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -24,5 +25,8 @@ export const Home = () => {
             <ProjectsSection/>
             <ContactSection/>
         </main>
+
+        {/* Footer Content*/}
+        <Footer />
     </div>;
 }
