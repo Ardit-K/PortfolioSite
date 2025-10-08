1dd7e5490e6a91d168a0e7ea13f5556c4fe98fdc
Ardit-K <arditkoti7192@gmail.com>
2025-10-08 12:03:20 -0400

    Removed Light Theme

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -23,9 +23,6 @@ return <section id="about" className="py-24 px-4 relative">
                     <a href="#contact" className="cosmic-button">
                         Contact Me
                     </a>
-                    {/* <a href="\dist\CS Resume Ardit Koti.pdf" download="CS Resume Ardit Koti.pdf" className="cosmic-button px-6 py-2 rounded-full border border-primary hover:bg-primary/10 transition-colors duration-300">
-                        Download CV
-                    </a> */}
                 </div>
             </div>
             <div className="grid grid-cols-1 gap-6">
